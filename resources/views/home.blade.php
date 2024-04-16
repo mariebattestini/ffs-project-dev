0c3664cb97f4f3b849c9250e16a18da045636d4d
Marie-Sophie BATTESTINI <marisofi1403@gmail.com>
2024-04-16 10:34:10 +0200

    register / login

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,5 +11,6 @@
         <img src="https://ffspeleo.fr/csx/scripts/resizer.php?filename=CHARTEX%2Flogobighome%2F68%2F99%2Ffiz5cgpdphyq&mime=image%252Fsvg%252Bxml&originalname=ffs-bottom-logo.svg&geometry=640x480%3E" alt="logo">
         <img src="https://ffspeleo.fr/csx/scripts/resizer.php?filename=CHARTEX%2Flogmd%2F68%2F99%2Ffiz5cgpdphyq&mime=image%252Fsvg%252Bxml&originalname=voyagez.svg&geometry=640x480%3E" alt="image">
     </nav>
+    
 </body>
 </html>
