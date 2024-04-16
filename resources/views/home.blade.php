65d7af1183f62cec7099b6705a48742aebc54484
Marie-Sophie BATTESTINI <marisofi1403@gmail.com>
2024-04-16 10:15:12 +0200

    beggining

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FFS</title>
+    <link rel="stylesheet" href="resources/css/app.css">
+</head>
+<body>
+    <nav>
+        <img src="https://ffspeleo.fr/csx/scripts/resizer.php?filename=CHARTEX%2Flogobighome%2F68%2F99%2Ffiz5cgpdphyq&mime=image%252Fsvg%252Bxml&originalname=ffs-bottom-logo.svg&geometry=640x480%3E" alt="logo">
+        <img src="https://ffspeleo.fr/csx/scripts/resizer.php?filename=CHARTEX%2Flogmd%2F68%2F99%2Ffiz5cgpdphyq&mime=image%252Fsvg%252Bxml&originalname=voyagez.svg&geometry=640x480%3E" alt="image">
+    </nav>
+</body>
+</html>
